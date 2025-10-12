d8a7d6ea1da7c68dadfc63aaf9ca0c2c5f843191
Carlos Augusto <168469953+gutoteix@users.noreply.github.com>
2025-10-12 17:52:14 +0000

    todas as tarefas finalizadas

--- myrep/poo/camisa/py/draft.py
+++ myrep/poo/camisa/py/draft.py
@@ -1,27 +1,35 @@
 class Roupa:
     def __init__(self):
         self.__tamanho = ""
-    
+
     def __str__(self):
-         return f"O tamanho da sua roupa é: {self.__tamanho}"
-    
-    def set_tamanho(self, valor: int) -> None: 
-                if valor != "PP" and valor != "P" and valor != "M" and valor != "G" and valor != "GG" and valor != "XG":
-                    print("Valor não permitido! Apenas PP, P, M, G, GG e XG")
-                    return
-                
-                self.__tamanho = valor
-    def get_tamanho(self) -> int: 
+        return f"O tamanho da sua roupa é: {self.__tamanho}"
+
+    def set_tamanho(self, valor: str) -> bool:
+        tamanhos_valido = ["PP", "P", "M", "G", "GG", "XG"]
+
+        if valor not in tamanhos_valido:
+            print("Valor não permitido! Apenas PP, P, M, G, GG e XG.")
+            return False 
+
+        self.__tamanho = valor
+        return True  
+
+    def get_tamanho(self) -> str:
         return self.__tamanho
 
+
 def main():
+    roupa = Roupa() 
+
     while True:
-        roupa = Roupa()
-    
-        print("Informe o tamanho da roupa")
-        line = input()
-        roupa.set_tamanho(line)
-        
-        print(roupa)
-        break
+        print("Informe o tamanho da roupa:")
+        line = input().strip().upper()  
+
+        if roupa.set_tamanho(line): 
+            break
+            
+    print(roupa)
+
+
 main()
---
+++ myrep/poo/roupa/py/draft.py
@@ -0,0 +1,34 @@
+class Roupa:
+    def __init__(self):
+        self.__tamanho = ""  
+
+    def set_tamanho(self, valor: str) -> None:
+        tamanhos_validos = ["PP", "P", "M", "G", "GG", "XG"]
+        if valor not in tamanhos_validos:
+            print("fail: Valor inválido, tente PP, P, M, G, GG ou XG")
+            return
+        self.__tamanho = valor
+
+    def get_tamanho(self) -> str:
+        return self.__tamanho
+
+
+def main():
+    roupa = Roupa()  
+
+    while True:
+        line = input().strip()
+        if not line:
+            continue
+        print(f"${line}")
+        parts = line.split()
+
+        if parts[0] == "end":
+            break
+        elif parts[0] == "show":
+            print(f"size: ({roupa.get_tamanho()})")
+        elif parts[0] == "size":
+            tamanhoEscolhido = parts[1]
+            roupa.set_tamanho(tamanhoEscolhido)
+    
+main()
